e1a9bf18caa590cd54c827d04e8b43479d323423
lucybaby0102 <lucybaby0102@gmail.com>
2025-02-27 14:39:24 +0800

    upload

--- gulpfile.js
+++ gulpfile.js
@@ -14,23 +14,35 @@ gulp.task('pug', function () {
     .pipe(browserSync.stream());
 });
 
+
 // Sass 編譯
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(plumber({ errorHandler: notify.onError('Sass Error: <%= error.message %>') }))
-    .pipe(sass())
-    .pipe(gulp.dest('./dist/css'))
+  return gulp.src('./src/sass/**/*.sass')  // 來源是 Sass 檔案
+    .pipe(plumber({ errorHandler: notify.onError('Sass Error: <%= error.message %>') }))  
+    .pipe(sass())  // 編譯 Sass 成 CSS
+    .pipe(gulp.dest('./dist/css'))  // 輸出到 dist/css 資料夾
     .pipe(browserSync.stream());
 });
 
-// 監聽檔案變化 & 自動刷新
-gulp.task('watch', function () {
-  browserSync.init({ server: { baseDir: './dist' } });
-
-  gulp.watch('./src/pug/**/*.pug', gulp.series('pug'));
-  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+//處理圖片和其他靜態資源
+gulp.task('images', function () {
+  return gulp.src('./src/images/**/*')  // 來源資料夾
+    .pipe(gulp.dest('./dist/images'));  // 複製到 dist/images 資料夾
 });
 
-// 預設執行
-gulp.task('default', gulp.series(gulp.parallel('pug', 'sass'), 'watch'));
+
+// 監聽檔案變化 & 自動刷新
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: './dist'  // 設定 BrowserSync 伺服器根目錄
+    }
+  });
+
+  gulp.watch('./src/pug/**/*.pug', gulp.series('pug'));  // 監聽 Pug 變化
+  gulp.watch('./src/sass/**/*.sass', gulp.series('sass'));  // 監聽 Sass 變化
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);  // 監聽 HTML 變化
+});
+
+// 預設任務：執行所有任務
+gulp.task('default', gulp.series('sass', 'pug', 'watch'));
